b8f7d2472788e0d1acf6ad32f5c47b168702a71b
samuelfer <samuelfesant@gmail.com>
2023-01-07 23:23:34 -0300

    feat: add parâmetro sucess=false na mensagens de erros

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,8 +1,10 @@
 class AppError {
   public readonly message: string;
   public readonly statusCode: number;
+  public readonly success: boolean;
 
   constructor(message: string, statusCode = 400) {
+    this.success = false;
     this.message = message;
     this.statusCode = statusCode;
   }
