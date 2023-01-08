4050a280ed293a4144ae9f1fa1ff1c7992eea5e2
samuelfer <samuelfesant@gmail.com>
2023-01-07 23:20:57 -0300

    Refactor no service e repository de user

--- src/modules/users/services/UserService.ts
+++ src/modules/users/services/UserService.ts
@@ -5,16 +5,20 @@ import User from '../typeorm/entities/User';
 import UserRepository from '../typeorm/repositories/UserRepository';
 
 class UserService {
-  private repository: UserRepository;
+  private userRepository;
+
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
 
   public async getAll(): Promise<User[]> {
-    const users = await this.repository.find();
+    const users = await this.userRepository.findAll();
 
     return users;
   }
 
   public async getById(id: string): Promise<User> {
-    const user = await this.repository.findOneBy({ id });
+    const user = await this.userRepository.findById(id);
 
     if (!user) {
       throw new AppError('User not found.');
@@ -28,47 +32,45 @@ class UserService {
     email,
     password,
   }: IRequestCreateUpdate): Promise<User> {
-    const userExists = await this.repository.findByEmail(email);
+    const userExists = await this.userRepository.findByEmail(email);
 
     if (userExists) {
-      throw new AppError('Email address already used');
+      throw new AppError('Email address already used.');
     }
-    const user = this.repository.create({
+    const user = await this.userRepository.save({
       name,
       email,
       password,
     });
-
-    await this.repository.save(user);
     return user;
   }
 
-  public async update({
-    id,
-    name,
-    email,
-    password,
-  }: IRequestCreateUpdate): Promise<User> {
-    const user = await this.repository.findOneBy({ id });
+  // public async update({
+  //   id,
+  //   name,
+  //   email,
+  //   password,
+  // }: IRequestCreateUpdate): Promise<User> {
+  //   const user = await this.repository.findById(id);
 
-    if (!user) {
-      throw new AppError('User not found.');
-    }
+  //   if (!user) {
+  //     throw new AppError('User not found.');
+  //   }
 
-    const userExists = await this.repository.findByName(name);
+  //   const userExists = await this.repository.findByName(name);
 
-    if (userExists) {
-      throw new AppError('There is already one user with this name');
-    }
+  //   if (userExists) {
+  //     throw new AppError('There is already one user with this name');
+  //   }
 
-    user.name = name;
-    user.email = email;
-    user.password = password;
+  //   user.name = name;
+  //   user.email = email;
+  //   user.password = password;
 
-    await this.repository.save(user);
+  //   await this.repository.save(user);
 
-    return user;
-  }
+  //   return user;
+  // }
 }
 
 export default UserService;
--- src/modules/users/typeorm/repositories/UserRepository.ts
+++ src/modules/users/typeorm/repositories/UserRepository.ts
@@ -1,10 +1,23 @@
+import { AppDataSource } from '@shared/typeorm';
 import { Repository } from 'typeorm';
+import IRequestCreateUpdate from '../entities/interfaces/IRequestCreateUpdate';
 
 import User from '../entities/User';
 
-class UserRepository extends Repository<User> {
+class UserRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(User);
+  }
+
+  public async findAll(): Promise<User[]> {
+    const users = await this.repository.find();
+    return users;
+  }
+
   public async findByName(name: string): Promise<User | null> {
-    const user = await this.findOne({
+    const user = await this.repository.findOne({
       where: {
         name,
       },
@@ -13,7 +26,7 @@ class UserRepository extends Repository<User> {
   }
 
   public async findById(id: string): Promise<User | null> {
-    const user = await this.findOne({
+    const user = await this.repository.findOne({
       where: {
         id,
       },
@@ -22,12 +35,14 @@ class UserRepository extends Repository<User> {
   }
 
   public async findByEmail(email: string): Promise<User | null> {
-    const user = await this.findOne({
-      where: {
-        email,
-      },
-    });
+    const user = await this.repository.findOneBy({ email });
+
     return user;
+  }
+
+  public async save(user: IRequestCreateUpdate): Promise<User> {
+    const userSaved = await this.repository.save(user);
+    return userSaved;
   }
 }
 
