63d5e525f34210c955f9e95ddef66694b5c0bbec
samuelfer <samuelfesant@gmail.com>
2023-01-07 23:22:44 -0300

    feat: import routes de users

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import userRouter from '@modules/users/routes/user.routes';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello Marha!' });
 });
+routes.use('/users', userRouter);
 
 export default routes;
